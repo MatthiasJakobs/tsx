6d2d63581548fc998a386e3b153705939385850a
Anne-Kathrin Stockey <stockey@ls8dl001.cs.tu-dortmund.de>
2023-02-10 10:37:53 +0100

    integrated annotations

--- tsx/datasets/piecewise_dataloading.py
+++ tsx/datasets/piecewise_dataloading.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from torch.utils.data import TensorDataset
+from tsx.utils import to_random_state
 
 # loading dataset
 def load_piecewise_sinusoidal(t0=96, n=1000, return_numpy=False, return_torch=False, random_state=0):
@@ -11,20 +12,19 @@ def load_piecewise_sinusoidal(t0=96, n=1000, return_numpy=False, return_torch=Fa
     if ps.return_numpy:
         return ps.x, ps.fx, ps.masks
     if ps.return_torch:
-        # TODO: Warum gibst du hier nur den ersten Datenpunkt immer zurück? Du kannst auch `torch.from_numpy` komplett auf ps.x / ps.fx aufrufen
-        return TensorDataset(torch.from_numpy(ps.x[0,:]), torch.from_numpy(ps.fx[0,:]), torch.from_numpy(ps.masks))
-        #return torch.from_numpy(ps.x[0,:]), torch.from_numpy(ps.fx[0,:]), torch.from_numpy(ps.masks)
+        return TensorDataset(torch.from_numpy(ps.x), torch.from_numpy(ps.fx), torch.from_numpy(ps.masks))
+        #return torch.from_numpy(ps.x), torch.from_numpy(ps.fx), torch.from_numpy(ps.masks)
     raise Exception("return type needed, set return_numpy oder return_torch on true depending which type is needed")
 
 # class for generatig dataset
-class PiecewieseSinusoidal(): # Comment: Ich hatte die Konvention Klassen immer in Camel-Case zu schreiben ;)
+class PiecewieseSinusoidal():
 
     def __init__(self, t0=96, n=1000, return_numpy=False, return_torch=False, random_state=0):
         self.t0 = t0
         self.n = n
         self.return_numpy = return_numpy
         self.return_torch = return_torch
-        self.rng = self.to_random_state(random_state)
+        self.rng = to_random_state(random_state)
 
         #timepoints
         self.x = np.vstack(self.n*[np.expand_dims(np.arange(0,self.t0+24).astype(float),0)])
@@ -39,17 +39,9 @@ class PiecewieseSinusoidal(): # Comment: Ich hatte die Konvention Klassen immer 
             np.expand_dims(part4,1)*np.sin(np.pi*self.x[0,t0:t0+24]/6)+72
         ])
 
-        # TODO: Ist das richtig, dass hier nur ein wert aus der normalverteilung gesampelt werden soll?
-        self.fx = self.fx + self.rng.normal()
+        self.fx = self.fx + self.rng.normal(size=self.fx.shape)
 
         self.masks = self._generate_square_subsequent_mask()
-
-    # TODO: Ich hatte inzwischen auf dem master branch unter `utils.py` auch eine `to_random_state` implementiert. Kannst du den master hier bei dir rein mergen und dann mit `from tsx.utils import to_random_state` laden?
-    # define randomstate
-    def to_random_state(self, rs):
-        if not isinstance(rs, np.random.RandomState):
-            rs = np.random.RandomState(rs)
-        return rs
 
     # generate mask
     def _generate_square_subsequent_mask(self):
