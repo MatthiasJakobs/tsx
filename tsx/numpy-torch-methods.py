27ad7e743d470150f0cd6df609fd9b674d876566
Anne-Kathrin Stockey <stockey@ls8dl001.cs.tu-dortmund.de>
2023-02-24 13:25:45 +0100

    finished numpy-torch-methods

--- tsx/numpy-torch-methods.py
+++ tsx/numpy-torch-methods.py
@@ -32,5 +32,46 @@ def argsort(X, axis=-1, kind=None, order=None, descending=False):
         return torch.argsort(X, dim=axis, descending=descending)
     return np.argsort(X, axis=axis, kind=kind, order=order) #X needs to be arraylike, everything is accepted
 
+#independent method for unsqueeze
+def unsqueeze(X, axis):
+    if isinstance(X, torch.Tensor):
+        return torch.unsqueeze(X, axis)
+    return np.expand_dims(X, axis)
 
-#TODO unsqueeze, zeros, zeros_like, one, one_like, sqrt, log, ...
+#independent method for sqrt
+def sqrt(X, out=None, where=True, **kwargs):
+    if isinstance(X, torch.Tensor):
+        return torch.sqrt(X, out=out)
+    return np.sqrt(X, out=out, where=where, **kwargs)
+
+#independent method for log
+def log(X, out=None, where=True, **kwargs):
+    if isinstance(X, torch.Tensor):
+        return torch.log(X, out=out)
+    return np.log(X, out=out, where=where, **kwargs)
+
+#independent method for zeros
+def zeros(shape, framework='numpy', dtype=None, order='C', like=np.zeros(0), out=None, layout=torch.strided, device=None, requires_grad=False):
+    if framework == 'torch':
+        return torch.zeros(shape, out=out, dtype=dtype, layout=layout, device=device, requires_grad=requires_grad)
+    if isinstance(dtype, type(None)): # np.zeros takes default dtype float not None
+        dtype = float
+    return np.zeros(shape, dtype=dtype, order=order, like=like)
+
+#independent method for zeros_like
+def zeros_like(data,*args, framework='numpy', dtype=None, layout=torch.strided, device=None, requires_grad=False, memory_format=torch.preserve_format, order='K', subok=True, newshape=None):
+    if framework == 'torch':
+        return torch.zeros_like(data, *args, dtype=dtype, layout=layout, device=device, requires_grad=requires_grad, memory_format=memory_format)
+    return np.zeros_like(data, dtype=dtype, order=order, subok=subok, shape=newshape)
+
+#independent method for ones
+def ones(shape, framework='numpy', dtype=None, order='C', like=np.zeros(0), out=None, layout=torch.strided, device=None, requires_grad=False):
+    if framework == 'torch':
+        return torch.ones(shape, out=out, dtype=dtype, layout=layout, device=device, requires_grad=requires_grad)
+    return np.ones(shape, dtype=dtype, order=order, like=like) # np.ones takes default dtype None
+
+#independent method for ones_like
+def ones_like(data, framework='numpy', dtype=None, layout=torch.strided, device=None, requires_grad=False, memory_format=torch.preserve_format, order='K', subok=True, newshape=None):
+    if framework == 'torch':
+        return torch.ones_like(data, dtype=dtype, device=device, layout=layout, requires_grad=requires_grad, memory_format=memory_format)
+    return np.ones_like(data, dtype=dtype, order=order, subok=subok, shape=newshape)
