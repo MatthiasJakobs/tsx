fdbebc9af04331980e8ec460d8e06cbc6cf486a8
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2023-08-09 11:58:35 +0200

    try importing tsx manually

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import tsx
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
