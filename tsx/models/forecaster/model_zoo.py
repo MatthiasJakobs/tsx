944daa0e369464a0af31ac8ed46b03f0d60c6397
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2024-10-07 13:46:51 +0200

    return pgsm conforming model

--- tsx/models/forecaster/model_zoo.py
+++ tsx/models/forecaster/model_zoo.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -24,31 +25,57 @@ def get_fcn(L, H, n_hidden=32, depth=3):
     return nn.Sequential(*model)
 
 def get_1d_cnn(L, H, n_channels=1, depth_feature=2, depth_classification=2, n_hidden_neurons=32, n_hidden_channels=16):
-    model = nn.ModuleList()
+    class CNN(nn.Module):
+        
+        def __init__(self):
+            super().__init__()
+            self.feature_extractor = nn.ModuleList()
+            self.forecaster = nn.ModuleList()
 
-    for i in range(depth_feature):
-        if i == 0:
-            model.append(nn.Conv1d(n_channels, n_hidden_channels, kernel_size=3, padding='same'))
-            model.append(nn.BatchNorm1d(n_hidden_channels))
-            model.append(nn.ReLU())
-        else:
-            model.append(nn.Conv1d(n_hidden_channels, n_hidden_channels, kernel_size=3, padding='same'))
-            model.append(nn.BatchNorm1d(n_hidden_channels))
-            model.append(nn.ReLU())
+            for i in range(depth_feature):
+                if i == 0:
+                    self.feature_extractor.append(nn.Conv1d(n_channels, n_hidden_channels, kernel_size=3, padding='same'))
+                    self.feature_extractor.append(nn.BatchNorm1d(n_hidden_channels))
+                    self.feature_extractor.append(nn.ReLU())
+                else:
+                    self.feature_extractor.append(nn.Conv1d(n_hidden_channels, n_hidden_channels, kernel_size=3, padding='same'))
+                    self.feature_extractor.append(nn.BatchNorm1d(n_hidden_channels))
+                    self.feature_extractor.append(nn.ReLU())
 
-    model.append(nn.Flatten())
-
-    if depth_classification == 1:
-        model.append(nn.Linear(L * n_hidden_channels, H))
-    else:
-        for i in range(depth_classification):
-            if i == 0:
-                model.append(nn.Linear(L * n_hidden_channels, n_hidden_neurons))
-                model.append(nn.ReLU())
-            elif i == depth_classification-1:
-                model.append(nn.Linear(n_hidden_neurons, H))
+            self.forecaster.append(nn.Flatten())
+            if depth_classification == 1:
+                self.forecaster.append(nn.Linear(L * n_hidden_channels, H))
             else:
-                model.append(nn.Linear(n_hidden_neurons, n_hidden_neurons))
-                model.append(nn.ReLU())
+                for i in range(depth_classification):
+                    if i == 0:
+                        self.forecaster.append(nn.Linear(L * n_hidden_channels, n_hidden_neurons))
+                        self.forecaster.append(nn.ReLU())
+                    elif i == depth_classification-1:
+                        self.forecaster.append(nn.Linear(n_hidden_neurons, H))
+                    else:
+                        self.forecaster.append(nn.Linear(n_hidden_neurons, n_hidden_neurons))
+                        self.forecaster.append(nn.ReLU())
 
-    return nn.Sequential(*model)
+            self.feature_extractor = nn.Sequential(*self.feature_extractor)
+            self.forecaster = nn.Sequential(*self.forecaster)
+
+        def forward(self, X):
+            feats = self.feature_extractor(X)
+            out = self.forecaster(feats)
+            return out
+        
+        def predict(self, X):
+            is_numpy = isinstance(X, np.ndarray)
+            if is_numpy:
+                X = torch.from_numpy(X).float()
+
+            self.eval()
+            with torch.no_grad():
+                preds = self.forward(X)
+
+            if is_numpy:
+                preds = preds.numpy()
+
+            return preds
+    
+    return CNN()
