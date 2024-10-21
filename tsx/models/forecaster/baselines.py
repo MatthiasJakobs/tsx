c33127e4da7ce81b113b751ccf3d8234bf77523c
Matthias Jakobs <6453841+MatthiasJakobs@users.noreply.github.com>
2024-10-21 15:47:06 +0200

    specify all parameters in init

--- tsx/models/forecaster/baselines.py
+++ tsx/models/forecaster/baselines.py
@@ -112,8 +112,8 @@ class ProbQuant:
 
 class TableForestRegressor(RandomForestRegressor):
 
-    def __init__(self, *args, include_raw=False, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(n_estimators=100, criterion='squared_error', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=1.0, max_leaf_nodes=None, min_impurity_decrease=0.0, bootstrap=True, oob_score=False, n_jobs=None, random_state=None, verbose=0, warm_start=False, ccp_alpha=0.0, max_samples=None, monotonic_cst=None):
+        super().__init__(n_estimators=n_estimators, criterion=criterion, max_depth=max_depth, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, min_weight_fraction_leaf=min_weight_fraction_leaf, max_features=max_features, max_leaf_nodes=max_leaf_nodes, min_impurity_decrease=min_impurity_decrease, bootstrap=bootstrap, oob_score=oob_score, n_jobs=n_jobs, random_state=random_state, verbose=verbose, warm_start=warm_start, ccp_alpha=ccp_alpha, max_samples=max_samples, monotonic_cst=monotonic_cst)
         self.include_raw = include_raw
 
     def fit(self, X, y, **kwargs):
